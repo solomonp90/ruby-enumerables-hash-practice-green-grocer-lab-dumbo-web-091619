250d139c5a0ef49a3938ebfc6d0c84b6ce789809
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-03 14:28:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,7 +25,7 @@ item = coupon[:item]
 if cart[item] && cart[:item][:count] >= coupon[:num] && !cart["#{item}W/COUPON"]
 cart["#{item}W/COUPON"] =  {price: coupon[:cost], clearance:cart[item][:clearance] , count: coupon[:num]}
 cart[item][:count] -= coupone[:num]
-elsif coupon[:item][:count] >= coupon[:num] && cart["#{item}W/COUPON"]
+elsif cart[:item][:count] >= coupon[:num] && cart["#{item}W/COUPON"]
 cart["#{item} W/COUPON"][:count] += coupon[:num]
 cart[item][:count] -= coupon[:num]
     end
